6e87a3453eefc87adaca6f1cd37b7765f1da7714
tong <tong@disktree.net>
2017-09-08 20:23:37 +0200

    Cleanup

--- java/om/android/AppActivity.java
+++ java/om/android/AppActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
@@ -22,14 +23,17 @@ public abstract class AppActivity extends Activity {
         super.onCreate( savedInstanceState );
 
         this.requestWindowFeature( Window.FEATURE_NO_TITLE );
-        //this.getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
+        this.getWindow().addFlags( WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON );
 
-        setSystemUi( View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        systemUiFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 			| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 			| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 			| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 			| View.SYSTEM_UI_FLAG_FULLSCREEN
-			| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            | View.SYSTEM_UI_FLAG_IMMERSIVE
+			| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        setSystemUi();
 
     	webview = new WebView( AppActivity.this );
         webview.setBackgroundColor( 0xff000000 );
@@ -54,12 +58,13 @@ public abstract class AppActivity extends Activity {
 		//settings.setJavaScriptCanOpenWindowsAutomatically(true);
 		//settings.setSupportMultipleWindows(true);
 
-		initAPI();
+        initAPI();
+
+        setContentView( webview );
 
         //loadWebApp();
 		//webview.loadUrl( "file:///android_asset/index.html" );
 
-        setContentView( webview );
 
         final View decor = getWindow().getDecorView();
         decor.setOnSystemUiVisibilityChangeListener( new View.OnSystemUiVisibilityChangeListener() {
@@ -70,6 +75,7 @@ public abstract class AppActivity extends Activity {
                 }
             }
         });
+
     }
 
 	protected void initAPI() {
